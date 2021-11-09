5d4b79d94df8365075ea936fe8540ef7a855f29b
Arik <55418469+BBArikL@users.noreply.github.com>
2021-11-08 20:47:21 -0500

    Deleted Flask server

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-from keepalive import keep_alive  # imports the web server that pings the bot continually
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -9,8 +8,6 @@ client = commands.Bot(command_prefix='bd!')
 client.remove_command("help")  # Removes the default "help" function to replace it by our own
 
 print("Starting Bot...")
-
-# keep_alive()  # Keeps the bot alive
 
 # Loads all the cogs
 for filename in os.listdir('./Scripts'):
